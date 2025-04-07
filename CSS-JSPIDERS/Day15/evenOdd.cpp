c874139be945467b5cc344a022a8549045fb8b1c
srinadhkonduri <srinadhkonduri@gmail.com>
2025-04-07 11:12:47 +0530

    committed

---
+++ CSS-JSPIDERS/Day15/evenOdd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 12;
+    if (n % 2 == 0)
+    {
+        cout << "even";
+    }
+    else
+    {
+        cout << "odd";
+    }
+    
+    return 0;
+}
---
+++ CSS-JSPIDERS/Day15/positive_negative.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num = -23;
+    if (num > 0)
+    {
+        cout << "positive number";
+    }
+    else if (num < 0)
+    {
+        cout << "negative number";
+    }
+    else{
+        cout << "zer0";
+    }
+    
+    return 0;
+}
